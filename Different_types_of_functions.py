4d85eb89ddc16876b1a7531f30bb427d3e758bba
OxiSgen <60621483+OxiSgen@users.noreply.github.com>
2020-09-09 22:39:30 +0300

    Create Different_types_of_functions.py

---
+++ Different_types_of_functions.py
@@ -0,0 +1,30 @@
+import time
+
+
+def one(a, b, c):
+    return a + b + c
+
+
+def two(*args):
+    return args[0] + args[1] + args[2]
+
+
+def three(**args):
+    return args['a'] + args['b'] + args['c']
+
+
+count = 9000000
+_startTime = time.time()
+for num in range(count):
+    one(1, 2, 3)
+print("Positional test:", time.time() - _startTime)
+
+_startTime = time.time()
+for num in range(count):
+    two(1, 2, 3)
+print("args as list test:", time.time() - _startTime)
+
+_startTime = time.time()
+for num in range(count):
+    three(a=1, b=2, c=3)
+print("args as dict test:", time.time() - _startTime)
